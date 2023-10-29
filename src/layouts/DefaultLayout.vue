c5ce92ed0f0e8c56f8fecd2157593e7198d4278b
Romi Julianto <romyjulians@gmail.com>
2023-10-29 22:39:11 +0700

    feat: setting responsive in tablet and mobile

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,21 +1,51 @@
+<script setup lang="ts">
+import { ref, computed } from "vue";
+
+const isMenuOpen = ref<boolean>(false);
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value;
+};
+
+const menuClasses = computed(() => ({
+  hidden: !isMenuOpen.value,
+  "md:flex": true,
+  "space-x-10": true,
+  "text-white": true,
+}));
+
+const activityImageClasses = computed(() => ({
+  "w-full": true,
+  "h-52 md:h-96": true,
+  "object-cover": true,
+}));
+</script>
+
 <template>
   <div id="layout">
     <div id="nav">
-      <header class="header flex items-center justify-center bg-black p-7">
-        <div class="logo absolute justify-start left-4">
+      <header class="flex items-center justify-center p-4 bg-black md:p-7">
+        <div
+          class="justify-center md:absolute md:justify-start md:logo md:left-4 md:flex"
+        >
           <img
             alt="logo-kaleka"
             src="../assets/images/img-logo.png"
-            class="h-[50px] w-[150px] cursor-pointer"
+            href="#"
+            class="w-auto h-12 cursor-pointer md:h-16"
           />
         </div>
-        <div class="menu flex space-x-10 text-white">
+        <div class="absolute justify-end cursor-pointer md:hidden right-4">
+          <button @click="toggleMenu" class="text-white">☰</button>
+        </div>
+        <div :class="menuClasses">
           <div class="cursor-pointer font-bold text-[#F8BE1A]">BERANDA</div>
           <div class="cursor-pointer font-bold text-[#F8BE1A]">
             TENTANG KAMI
           </div>
         </div>
       </header>
+
       <div id="activities">
         <img
           alt="kaleka-activities"
