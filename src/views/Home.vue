e2acb87ba6bce9a9ef006ebc10b825985adadabd
Romi Julianto <romyjulians@gmail.com>
2023-10-29 11:07:09 +0700

    feat: init project, add tailwind, add open layers and setup router

---
+++ src/views/Home.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import OlMap from "ol/Map";
+import View from "ol/View";
+import TileLayer from "ol/layer/Tile";
+import { OSM } from "ol/source";
+import { onMounted, ref } from "vue";
+
+const map = ref<OlMap | null>(null);
+const VIEW = new View({
+  center: [117.25620141667574, -0.31222948067971146],
+  zoom: 5,
+  projection: "EPSG:4326",
+});
+
+const BASEMAP = new TileLayer({
+  source: new OSM(),
+});
+
+onMounted(async () => {
+  map.value = new OlMap({
+    target: "map",
+    layers: [BASEMAP],
+    view: VIEW,
+  });
+});
+</script>
+
+<template>
+  <div id="map" class="map"></div>
+</template>
+
+<style scoped>
+#map {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+}
+.ol-attribution {
+  display: none !important;
+}
+</style>
---
+++ src/views/User.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">User</h1>
+</template>
+
+<style scoped>
+</style>
